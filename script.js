9b0bf1a7959c6187e93ebb9e2c5e31ddfb64372d
Madness <97130349+blueMadnessDev@users.noreply.github.com>
2023-06-19 10:15:40 -0400

    Changed text and gradient!

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var scrollLocked = true;
 
 function handleInput(event) {
   inputText += event.key;
+  if (inputText === "amongus") {
+    outWithTheOldInWithTheNew()
+  }
   if (inputText === "qwerty") {
     var button = document.getElementById("invisible-button");
     button.style.display = "block";
@@ -121,105 +124,113 @@ function outWithTheOldInWithTheNew() {
   container6.style.width = "100%";
   document.body.appendChild(container6);
 
-  createButton(container1, "#0000FF", "#DDDDDD", "Browser", "https://cloudflare.rammerhead.org");
+  createButton(container1, "#0000ff", "Browser", "https://cloudflare.rammerhead.org");
 
-  createButton(container2, "#0004c2" , "#DDDDDD", "Dino game", "https://chromedino.com/");
-  createButton(container2, "#0004c2", "#DDDDDD", "Are you kidding me?", "https://turbowarp.org/836226987/embed");
-  createButton(container2, "#0004c2", "#DDDDDD", "Clicker Heroes" , "https://games-online.io/game/ClickerHeroes/");
-  createButton(container2, "#0004c2", "#DDDDDD", "FNF" , "https://static.playunblocked.com/2021/04/friday-night-funkin/");
-  createButton(container2, "#0004c2", "#DDDDDD", "Chess", "https://toytheater.com/chess.php");
-  createButton(container2, "#0004c2", "#DDDDDD", "Checkers", "https://toytheater.com/checkers.php");
-  createButton(container2, "#0004c2", "#DDDDDD", "Run 3", "https://unblocked1889.github.io/R033/tn6pS9dCf37xAhkJv/");
-
+  createButton(container2, "#0032ff", "Dino game", "https://chromedino.com/");
+  createButton(container2, "#0032ff", "Are you kidding me?", "https://turbowarp.org/836226987/embed");
+  createButton(container2, "#0032ff", "Clicker Heroes", "https://games-online.io/game/ClickerHeroes/");
+  createButton(container2, "#0032ff", "FNF", "https://static.playunblocked.com/2021/04/friday-night-funkin/");
+  createButton(container2, "#0032ff", "Chess", "https://toytheater.com/chess.php");
+  createButton(container2, "#0032ff", "Checkers", "https://toytheater.com/checkers.php");
+  createButton(container2, "#0032ff", "Run 3", "https://unblocked1889.github.io/R033/tn6pS9dCf37xAhkJv/");
   
+  createButton(container3, "#0064ff", "Subway Surfers", "https://superteamxx.github.io/Subway-Surfers/");
+  createButton(container3, "#0064ff", "GD", "https://turbowarp.org/105500895/embed");
+  createButton(container3, "#0064ff", "Youtube bypass", "https://speedtesting.herokuapp.com/videodrive/?m=Video_To_Drive");
+  createButton(container3, "#0064ff", "2d Minecraft", "https://turbowarp.org/10128407/embed");
+  createButton(container3, "#0064ff", "Mario", "https://jcw87.github.io/c2-smb1/");
+  createButton(container3, "#0064ff", "Bored button", "https://www.boredbutton.com/random");
   
-  createButton(container3, "#0008c2", "#DDDDDD", "Subway Surfers", "https://superteamxx.github.io/Subway-Surfers/");
-  createButton(container3, "#0008c2", "#DDDDDD", "GD", "https://turbowarp.org/105500895/embed");
-  createButton(container3, "#0008c2", "#DDDDDD", "Youtube bypass", "https://speedtesting.herokuapp.com/videodrive/?m=Video_To_Drive");
-  createButton(container3, "#0008c2", "#DDDDDD", "2d Minecraft", "https://turbowarp.org/10128407/embed");
-  createButton(container3, "#0008c2", "#DDDDDD", "Mario" , "https://jcw87.github.io/c2-smb1/");  
-  createButton(container3,  "#0008c2", "#DDDDDD", "Bored button" , "https://www.boredbutton.com/random");
+  createButton(container4, "#0095ff", "Riddle School", "https://sz-games.github.io/games/ridd");
+  createButton(container4, "#0095ff", "Riddle School 2", "https://sz-games.github.io/games/ridd2");
+  createButton(container4, "#0095ff", "Riddle School 3", "https://sz-games.github.io/games/ridd3");
+  createButton(container4, "#0095ff", "Riddle School 4", "https://sz-games.github.io/games/ridd4");
+  createButton(container4, "#0095ff", "Riddle School 5", "https://sz-games.github.io/games/ridd5");
+  createButton(container4, "#0095ff", "Riddle Transfer", "https://sz-games.github.io/games/ridd6");
+  createButton(container4, "#0095ff", "Riddle Transfer 2", "https://sz-games.github.io/games/ridd7");
+  
+  createButton(container5, "#00c8ff", "Smash Karts", "https://mr-funkinguy.github.io/75/projects/smashkarts/index.html");
+  createButton(container5, "#00c8ff", "Agar.io", "https://emupedia.net/emupedia-game-agar.io/");
+  createButton(container5, "#00c8ff", "Stack", "https://sz-games.github.io/games/stack");
+  
+  createButton(container6, "#00faff", "Crossy Road", "https://sz-games.github.io/cr/");
+  createButton(container6, "#00faff", "Fluids", "https://sz-games.github.io/storage3/1/");
+  createButton(container6, "#00faff", "Temple Run 2", "https://mr-funkinguy.github.io/Abc6782/games/temple-run-2/index.html");
+  createButton(container6, "#00faff", "Google Snake!", "https://mr-funkinguy.github.io/gfile/snake/index.html");
+  
+  function createButton(parent, backgroundColor, text, url) {
+    var button = document.createElement("button");
+    button.style.backgroundColor = backgroundColor;
+    button.style.padding = "10px";
+    button.style.borderRadius = "10px";
+    button.style.border = "none";
+    button.style.margin = "5px";
+    button.style.cursor = "pointer";
+    button.style.width = "100%";
+    button.style.height = "120px";
+    button.textContent = text;
+    button.onclick = function () {
+      clear();
+      open(url);
+    };
+    
+    var bgColor = backgroundColor.toLowerCase();
+    var textColor = getContrastingColor(bgColor);
+    button.style.color = textColor;
 
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle School" , "https://sz-games.github.io/games/ridd");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle School 2" , "https://sz-games.github.io/games/ridd2");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle School 3" , "https://sz-games.github.io/games/ridd3");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle School 4" , "https://sz-games.github.io/games/ridd4");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle School 5" , "https://sz-games.github.io/games/ridd5");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle Transfer" , "https://sz-games.github.io/games/ridd6");
-  createButton(container4,  "#000bc2", "#DDDDDD", "Riddle Transfer 2" , "https://sz-games.github.io/games/ridd7");
+    var observer = new ResizeObserver(function (entries) {
+      entries.forEach(function (entry) {
+        var buttonWidth = entry.contentRect.width;
+        var fontSize = Math.floor(buttonWidth / 14); // Adjust the division factor to scale the font size
+        button.style.fontSize = fontSize + "px";
+      });
+    });
+    parent.appendChild(button);
+    observer.observe(button);
+  
 
-  createButton(container5,  "#000fc2", "#DDDDDD", "Smash Karts" , "https://mr-funkinguy.github.io/75/projects/smashkarts/index.html");
-  createButton(container5,  "#000fc2", "#DDDDDD", "Agar.io" , "https://emupedia.net/emupedia-game-agar.io/");
-  createButton(container5,  "#000fc2", "#DDDDDD", "Stack" , "https://sz-games.github.io/games/stack");
-
-  createButton(container6,  "#0013c2", "#DDDDDD", "Crossy Road" , "https://sz-games.github.io/cr/");
-  createButton(container6,  "#0013c2", "#DDDDDD", "Fluids" , "https://sz-games.github.io/storage3/1/");
-  createButton(container6,  "#0013c2", "#DDDDDD", "Temple Run 2" , "https://mr-funkinguy.github.io/Abc6782/games/temple-run-2/index.html");
-  createButton(container6,  "#0013c2", "#DDDDDD", "Google Snake!" , "https://mr-funkinguy.github.io/gfile/snake/index.html");
-}
-
-function createButton(parent, backgroundColor, textColor, text, url) {
-var button = document.createElement("button");
-button.style.backgroundColor = backgroundColor;
-button.style.color = textColor;
-button.style.padding = "10px";
-button.style.borderRadius = "10px";
-button.style.border = "none";
-button.style.margin = "5px";
-button.style.cursor = "pointer";
-button.style.width = "100%";
-button.style.height = "120px";
-button.textContent = text;
-button.onclick = function () {
-  clear();
-  open(url);
-};
-
-
-// Automatically adjust font size based on button width
-var observer = new ResizeObserver(function (entries) {
-  entries.forEach(function (entry) {
-    var buttonWidth = entry.contentRect.width;
-    var fontSize = Math.floor(buttonWidth / 18); // Adjust the division factor to scale the font size
-    button.style.fontSize = fontSize + "px";
-  });
-});
-parent.appendChild(button);
-observer.observe(button);
-}
-
-
-
-function open(url) {
-var iframe = document.createElement("iframe");
-iframe.src = url;
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.position = "fixed";
-iframe.style.top = "0";
-iframe.style.left = "0";
-iframe.style.border = "none";
-document.body.appendChild(iframe);
-
-var button = document.createElement("button");
-button.id = "buttonback";
-button.style.backgroundColor = "#000fc2";
-button.style.color = "#DDDDDD";
-button.style.padding = "10px";
-button.style.borderRadius = "10px";
-button.style.border = "none";
-button.style.margin = "5px";
-button.style.cursor = "pointer";
-button.style.width = "17.25%";
-button.style.height = "3.45%";
-button.style.position = "fixed";
-button.style.bottom = "10px";
-button.style.left = "50%";
-button.style.transform = "translateX(-50%)";
-button.textContent = "Go back";
-button.onclick = function() {
-  outWithTheOldInWithTheNew();
-};
-
-document.body.appendChild(button);
+  }
+  
+ 
+  function getContrastingColor(backgroundColor) {
+    var r = parseInt(backgroundColor.substr(1, 2), 16);
+    var g = parseInt(backgroundColor.substr(3, 2), 16);
+    var b = parseInt(backgroundColor.substr(5, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? "#000000" : "#ffffff";
+  }
+  
+  function open(url) {
+    var iframe = document.createElement("iframe");
+    iframe.src = url;
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.left = "0";
+    iframe.style.border = "none";
+    document.body.appendChild(iframe);
+  
+    var button = document.createElement("button");
+    button.id = "buttonback";
+    button.style.backgroundColor = "#0095ff";
+    button.style.color = "#DDDDDD";
+    button.style.padding = "10px";
+    button.style.borderRadius = "10px";
+    button.style.border = "none";
+    button.style.margin = "5px";
+    button.style.cursor = "pointer";
+    button.style.width = "17.25%";
+    button.style.height = "3.45%";
+    button.style.position = "fixed";
+    button.style.bottom = "10px";
+    button.style.left = "50%";
+    button.style.transform = "translateX(-50%)";
+    button.textContent = "Go back";
+    button.onclick = function () {
+      outWithTheOldInWithTheNew();
+    };
+  
+    document.body.appendChild(button);
+  }
 }
